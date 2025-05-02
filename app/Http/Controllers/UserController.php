8ebc8f88e9512320549bc06374fcd69f4153c7da
unknown <klopster070@gmail.com>
2025-05-03 03:23:20 +0700

    second commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,54 @@ class UserController extends Controller
 
         return response()->json(['message' => 'User not found'], 404);
     }
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|string|max:20',
+        ]);
+
+        $user = User::create($validated);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $user
+        ], 201);
+    }
+    public function update(Request $request, $id)
+{
+    $user = User::find($id);
+    if (!$user) {
+        return response()->json(['message' => 'User not found'], 404);
+    }
+
+    $validated = $request->validate([
+        'name' => 'sometimes|required|string|max:255',
+        'email' => 'sometimes|required|email|unique:users,email,' . $id,
+        'phone' => 'sometimes|required|string|max:20',
+    ]);
+
+    $user->update($validated);
+
+    return response()->json([
+        'status' => 'success',
+        'data' => $user
+    ]);
+
+}
+public function destroy($id)
+{
+    $user = User::find($id);
+    if (!$user) {
+        return response()->json(['message' => 'User not found'], 404);
+    }
+
+    $user->delete();
+
+    return response()->json(['message' => 'User deleted successfully']);
+}
+
 
 
 }
